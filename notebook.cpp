25bcea81113f4305f620bfbdf8ba0a2465a73ac9
yang.liu <3285269068@qq.com>
2026-02-15 21:12:14 +0800

    初始提交

---
+++ notebook.cpp
@@ -0,0 +1,12 @@
+#include "notebook.h"
+#include "./ui_notebook.h"
+
+NoteBook::NoteBook(QWidget *parent) : QMainWindow(parent), ui(new Ui::NoteBook)
+{
+    ui->setupUi(this);
+}
+
+NoteBook::~NoteBook()
+{
+    delete ui;
+}
